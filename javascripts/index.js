da718fe385edee27a084a8b7d0baaab403e6d2cb
Ian Schreuder <ian@Josephine.local>
2009-09-27 11:53:46 -0600

    working drop down - needs a lot of styling

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,16 +1,7 @@
-$(document).ready(function(){
- 
-    //$('.down-list').width($('.dropdown-menu').width()-2);
- 
-    $('.dropdown-menu').hover(
-      function () {
-        $('.menu-first', this).addClass('slide-down');
-        $('.down-list', this).slideDown(100);
-      },
-      function () {
-        obj = this;
-        $('.down-list', this).slideUp(100, function(){ $('.menu-first', obj).removeClass('slide-down'); });
-      }
-    );
- 
+$(function(){  
+  $(".links li").hover(function(){  // onmouseover
+    $(this).addClass("menu_hover");
+  }, function(){                    // onmouseout
+    $(this).removeClass("menu_hover");
+  });
 });
